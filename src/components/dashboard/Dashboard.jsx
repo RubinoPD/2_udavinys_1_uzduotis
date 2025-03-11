268e521147059150b3ff04e461683e947a73c649
RubinoPD <robertaskask@gmail.com>
2025-03-11 19:07:02 +0200

    Update Dashboard.jsx to include recipe pages

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useAuth } from "../../contexts/AuthContext";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const { user } = useAuth();
@@ -10,10 +11,21 @@ const Dashboard = () => {
         <h3>Sveiki, {user.username}!</h3>
         <p>Jūsų el. paštas: {user.email}</p>
         <p>Vartotojo ID: {user.id}</p>
-        <p>
-          Čia galite patalpinti bet kokį apsaugotą turinį, kuris matomas tik
-          prisijungusiam vartotojui.
-        </p>
+      </div>
+
+      <h3 className="section-title">Receptų aplikacija</h3>
+      <div className="dashboard-actions">
+        <Link to="/recipes" className="dashboard-action-card">
+          <h4>Naršyti receptus</h4>
+          <p>Peržiūrėkite mūsų receptų kolekciją</p>
+          <span className="action-icon">🍳</span>
+        </Link>
+
+        <Link t="/favorites" className="dashboard-action-card">
+          <h4>Mėgstamiausi Receptai</h4>
+          <p>Peržiūrėkite savo išsaugotus receptus</p>
+          <span className="action-icon">❤️</span>
+        </Link>
       </div>
     </div>
   );
